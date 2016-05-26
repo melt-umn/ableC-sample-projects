65c0f2badb14117912fcc030ebaddddfc34a3b0d
Lucas Kramer <krame505@umn.edu>
2016-05-26 09:52:19 -0500

    Transparent prefixes work but require a lot of hacking

--- using_transparent_prefixes/compiler/Artifact.sv
+++ using_transparent_prefixes/compiler/Artifact.sv
@@ -3,14 +3,22 @@ grammar compiler;
 import edu:umn:cs:melt:ableC:concretesyntax as cst;
 import edu:umn:cs:melt:ableC:drivers:parseAndPrint;
 
-terminal Bogus_t 'bogus';
+
+--import edu:umn:cs:melt:exts:ableC:tables;
+--import bogus_table;
+
+terminal Bogus_t 'bogus' lexer classes {Ckeyword};
+
+disambiguate TableKwd_t, Table_t {
+  pluck Table_t;
+}
 
 parser extendedParser :: cst:Root {
   edu:umn:cs:melt:ableC:concretesyntax;
 
   edu:umn:cs:melt:exts:ableC:tables;
 
-  compiler; -- Need to include the current grammar for now to include any terminal defs
+  compiler; -- Need to include the current grammar for now to include any copper defs
   --bogus_table;
   bogus_table prefix TableKwd_t with Bogus_t;
 }
