a0e311896dd6b1e851a33068ba3bfab9c3b8ce84
Lucas Kramer <krame505@umn.edu>
2016-05-25 21:39:59 -0500

    Added test of most basic transparent prefixes

--- using_transparent_prefixes/compiler/Artifact.sv
+++ using_transparent_prefixes/compiler/Artifact.sv
@@ -3,13 +3,17 @@ grammar compiler;
 import edu:umn:cs:melt:ableC:concretesyntax as cst;
 import edu:umn:cs:melt:ableC:drivers:parseAndPrint;
 
+terminal Bogus_t 'bogus';
+
 parser extendedParser :: cst:Root {
   edu:umn:cs:melt:ableC:concretesyntax;
 
   edu:umn:cs:melt:exts:ableC:tables;
 
-  bogus_table;
-} 
+  compiler; -- Need to include the current grammar for now to include any terminal defs
+  --bogus_table;
+  bogus_table prefix TableKwd_t with Bogus_t;
+}
 
 function main
 IOVal<Integer> ::= args::[String] io_in::IO
