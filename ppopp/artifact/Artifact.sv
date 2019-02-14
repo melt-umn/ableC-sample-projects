49c565755ebba7f09bb8666b6b47bcbdda6b7d83
Travis Carlson <carl4980@umn.edu>
2019-02-14 09:03:39 -0600

    add ppopp examples copied from parallel-examples repo

---
+++ ppopp/artifact/Artifact.sv
@@ -0,0 +1,14 @@
+grammar artifact;
+
+import edu:umn:cs:melt:ableC:drivers:compile;
+
+construct ableC as
+edu:umn:cs:melt:ableC:concretesyntax
+translator using
+  edu:umn:cs:melt:exts:ableC:run;
+  edu:umn:cs:melt:exts:ableC:lvars;
+  edu:umn:cs:melt:exts:ableC:templating;
+
+  edu:umn:cs:melt:exts:ableC:run;
+  edu:umn:cs:melt:exts:ableC:tensorAlgebra;
+
