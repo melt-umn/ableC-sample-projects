35567b48eaf039e52f399175a5a1e0e9615411be
Lucas Kramer <krame505@umn.edu>
2020-02-04 13:16:01 -0600

    Changes to show of prefix seperator modifications

--- using_transparent_prefixes/alternate_separator/CompilerSpec.sv
+++ using_transparent_prefixes/alternate_separator/CompilerSpec.sv
@@ -1,17 +1,10 @@
 grammar alternate_separator;
-
--- This specification does not work!
--- See Silver issue #85
--- https://github.com/melt-umn/silver/issues/85
--- Specifying a prefix separator in a parser spec doesn't override the
--- one specified in the host language.  Instead is clashes with it.
-
-
 {- This specification composes two grammars that lead to a lexical
    ambiguity. 
 
-   This example specifies a *prefix separator* explicitly to override
-   the default separator of `::` specified in the host language.
+   In this example one grammar uses a different prefix seperator for
+   some terminals than the default separator of `::` specified in the
+   host language.
  -}
  
 import edu:umn:cs:melt:ableC:concretesyntax as cst;
@@ -21,21 +14,20 @@ parser extendedParser :: cst:Root {
   edu:umn:cs:melt:ableC:concretesyntax;
 
   -- The two lines below define the transparent prefix for
-  -- the two different extensin grammars being used in this
+  -- the two different extension grammars being used in this
   -- composition.
+  -- Note that bogus_table_separator exports multiple marking terminals
+  -- with different prefix seperators, so the prefix must be specified
+  -- specifically for each terminal
 
   edu:umn:cs:melt:exts:ableC:tables prefix with "CT";
-  bogus_table prefix with "BT";
-
-  -- The following line overrides the prefix separator specified in
-  -- ableC with a new one.
-  prefix separator "@";
+  bogus_table_separator prefix bogus_table_separator:TableKwd_t with "BT";
 
   -- Indicate that 'table' is to be scanned as the terminal from the
   -- `tableExpr` grammar.  It is preferred over the one from the
   -- `bogus_table` grammar.
   prefer edu:umn:cs:melt:exts:ableC:tables:concretesyntax:Table_t
-    over bogus_table:TableKwd_t, cst:Identifier_t, cst:TypeName_t;
+    over bogus_table_separator:TableKwd_t, cst:Identifier_t, cst:TypeName_t;
 }
 
 
---
+++ using_transparent_prefixes/bogus_table_separator/ConcreteSyntax.sv
@@ -0,0 +1,25 @@
+grammar bogus_table_separator;
+
+imports edu:umn:cs:melt:ableC:concretesyntax as cnc;
+
+lexer class OtherSeperatorThings prefix separator "@";
+
+marking terminal TableKwd_t 'table' lexer classes {OtherSeperatorThings};
+
+marking terminal RandomUnusedKeyword_t 'blah' lexer classes {Keyword};
+
+disambiguate TableKwd_t, cnc:Identifier_t {
+  pluck TableKwd_t;
+}
+
+disambiguate TableKwd_t, cnc:Identifier_t, cnc:TypeName_t {
+  pluck TableKwd_t;
+}
+
+concrete production table_thing_c
+top::cnc:PrimaryExpr_c ::= 'table' x::cnc:Constant_c ',' y::cnc:Constant_c ',' z::cnc:Constant_c
+{
+  top.ast = table_thing(x.ast, y.ast, z.ast, location=top.location);
+}
+
+
